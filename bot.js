830512ce474398131d86fb914a25b87582ccfe42
uae004 <54192068+uae004@users.noreply.github.com>
2019-08-17 03:25:17 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("612054535657095211")
+setInterval(function() {
+channel.send(`hacker gg`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
